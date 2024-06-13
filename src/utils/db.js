f5a3d22ca8185cf75280cedb90e8a6649bfd65ae
FaisalKhan-8 <mdfaisalkhan1500@gmail.com>
2024-06-13 23:49:05 +0530

    cc

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 export const DataBase = async()=>{
     try {
-        await mongoose.connect(process.env.MONGODB_URI)
-        console.log("Connected to mongodb");
+      const conectionInstance =  await mongoose.connect(process.env.MONGODB_URI)
+        console.log(`Connected to mongodb at ${conectionInstance.connection.host}`);
     } catch (error) {
         console.log(error);
     }
